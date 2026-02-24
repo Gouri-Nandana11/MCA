ae54cce9e6b1fd56b9fb9c85827daefd3407609f
Gouri Nandana <gourinandanam11@gmail.com>
2026-02-24 23:20:19 +0530

    Add files via upload

---
+++ Java/TestA.java
@@ -0,0 +1,11 @@
+import arthm.*;
+public class TestA{
+    public static void main(String[] args) {
+        System.out.println("..CALCULATOR..");
+        op c=new op();
+        c.add(7,5);
+        c.sub(3,2);
+            
+        
+    }
+}
